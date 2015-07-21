66d23fb3102f2f92f853952f6d279b717c9dc308
SolomonCaceres <solarsol@gmail.com>
2015-07-20 23:58:52 -0400

    Initial Commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,103 @@
-body{
 
-	backgroundcolor: orange;
-}
+$(document).ready(function() {
+     /* alert('Oh, hi there, world');*/
+   
+
+
+
+
+/* Reset function For Favorites*/
+$('#clear-form').on('click', function()
+    {
+        $('#FAVFORM').find('input:text').val('');
+			$("#AFavbox").hide();
+			$("#BFavbox").hide();
+			$("#CFavbox").hide();
+			$("#DFavbox").hide();
+			$("#EFavbox").hide();
+
+});
+
+
+
+
+
+
+/*This hides all the form text boxes for the Favorites menu*/
+$(".MAINFAVORITEBOX").hide();
+$("#AFavbox").hide();
+$("#BFavbox").hide();
+$("#CFavbox").hide();
+$("#DFavbox").hide();
+$("#EFavbox").hide();
+
+
+
+
+
+
+
+/* This is the mouse enter and mouse down function for the FAVORITES Button*/
+
+$(".Favbutton").mouseenter(function(){
+
+	$(".MAINFAVORITEBOX").show("145");
+
+});
+
+$(".Favbutton").mousedown(function(){
+
+	$(".MAINFAVORITEBOX").hide("145");
+});
+
+
+
+
+
+
+
+
+/*These are the text boxes within the MAINFAVORITEBOX Div*/
+
+$( "body" ).on( "click", "#Favbox", function() {
+  $("#AFavbox").show();
+
+});
+
+$( "body" ).on( "click", "#AFavbox", function() {
+  $("#BFavbox").show();
+
+
+});
+
+$( "body" ).on( "click", "#BFavbox", function() {
+  $("#CFavbox").show();
+
+
+});
+
+$( "body" ).on( "click", "#CFavbox", function() {
+  $("#DFavbox").show();
+
+
+});
+
+$( "body" ).on( "click", "#DFavbox", function() {
+  $("#EFavbox").show();
+
+
+});
+
+
+$( "body" ).on( "click", "#EFavbox", function() {
+  $("#FFavbox").show();
+
+
+});
+
+
+
+
+
+
+});
